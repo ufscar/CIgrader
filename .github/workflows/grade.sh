1296f9ec789d43c0d08091c585c41022e8e7e401
afsmaira <andre.smaira@alumni.usp.br>
2021-08-10 17:11:15 -0300

    Test event info

--- .github/workflows/grade.sh
+++ .github/workflows/grade.sh
@@ -1,4 +1,7 @@
-curl "${PROF_GITHUB}" 2>/dev/null | grep lista
+URL="${PROF_GITHUB}"
+URI=$( echo "${URL}" | sed "s,https://github.com/,,g" )
+echo "${URI}"
+
 
 #readarray -t added_modified_files <<<"$(jq -r '.[]' <<<'${{ steps.files.outputs.added_modified }}')"
 #for added_modified_file in "${added_modified_files[@]}"; do
