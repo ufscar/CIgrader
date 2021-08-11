acc9be8988d80ca0efde02eb65b29938932a9402
afsmaira <andre.smaira@alumni.usp.br>
2021-08-10 21:14:10 -0300

    Test event info

--- .github/workflows/grade.sh
+++ .github/workflows/grade.sh
@@ -4,6 +4,7 @@ CONTENTS="https://api.github.com/repos/${URI}/contents/"
 PROF_WORKS=$( curl "${CONTENTS}" 2>/dev/null | jq -r '.[] | select(.type == "dir") | .name' )
 COMMIT_TIME='${{ steps.author-date.outputs.result }}'
 echo "${COMMIT_TIME}"
+exit 1
 
 #for work in "${AUX[@]}";
 #do
@@ -17,19 +18,22 @@ echo "${COMMIT_TIME}"
 #done;
 
 
-#readarray -t added_modified_files <<<"$(jq -r '.[]' <<<'${{ steps.files.outputs.added_modified }}')"
-#for added_modified_file in "${added_modified_files[@]}";
-#do
-#  work=$( echo "${added_modified_file}" | cut -d "/" -f1 )
-#  if [[ " ${PROF_WORKS[@]} " =~ " ${work} " ]];
-#  then
-#    grade=0;
-#    if [[ -z $( curl "${CONTENTS}/${work}" 2>/dev/null | jq -r '.[] | select(.name == "due_to.txt")' 2>/dev/null ) ]];
-#    then
-#      grade=1;
-#    else [[ date +"$( cat teste )" ]];
-#    then
-#      grade=1;
-#    fi;
-#  fi;
-#done
+readarray -t added_modified_files <<<"$(jq -r '.[]' <<<'${{ steps.files.outputs.added_modified }}')"
+for added_modified_file in "${added_modified_files[@]}";
+do
+  work=$( echo "${added_modified_file}" | cut -d "/" -f1 )
+  if [[ " ${PROF_WORKS[@]} " =~ " ${work} " ]];
+  then
+    grade=0;
+    if [[ -z $( curl "${CONTENTS}/${work}" 2>/dev/null | jq -r '.[] | select(.name == "due_to.txt")' 2>/dev/null ) ]];
+    then
+      grade=1;
+    else
+      date=$( date +"$( curl "${CONTENTS}/${work}/due_to.txt" 2>/dev/null | jq -r .content | base64 -d - )" )
+      if [[ ${date} < ${COMMIT_TIME} ]];
+      then
+        grade=1;
+      fi;
+    fi;
+  fi;
+done
